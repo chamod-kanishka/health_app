f5a77efa9cea4127bf34f9bb934ddafc8aa78f34
chamod-kanishka <chamodkanishka519@gmail.com>
2024-04-16 06:16:02 +0530

    "health app is initialized"

---
+++ lib/health_app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/health_home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Health App',
+      home: Scaffold(
+        body: Center(
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: HealthHomeScreen(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'health_app.dart';
+
+void main() {
+  runApp(MyApp());
+}
